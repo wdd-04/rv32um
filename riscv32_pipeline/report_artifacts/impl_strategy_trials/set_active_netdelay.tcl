40ff028770cf5e0b24fa20110fafe4b45dc43242
wdd-04 <2443653661@qq.com>
2026-08-17 21:09:55 +0800

    Delete riscv32_pipeline/report_artifacts directory

--- riscv32_pipeline/report_artifacts/impl_strategy_trials/set_active_netdelay.tcl
+++
@@ -1,5 +0,0 @@
-open_project D:/riscv32_pipeline_cache/riscv32_pipeline.xpr
-current_run -implementation [get_runs impl_trial_netdelay_high]
-save_project
-puts "ACTIVE_IMPL=[current_run -implementation]"
-exit
